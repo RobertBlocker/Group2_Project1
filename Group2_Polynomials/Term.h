d25fec37c7002115a6e4428351109a181846798a
bryce0110 <bdt0110@yahoo.com>
2022-10-19 19:17:20 -0500

    Basic Term Class

--- Group2_Polynomials/Term.h
+++ Group2_Polynomials/Term.h
@@ -6,5 +6,36 @@
 
 using namespace std;
 
+class Term {
+public:
+    
+    // Default Constructor
+    Term(int, int);
+
+    // Operators
+    friend bool operator < (const Term&, const Term&);
+    friend bool operator <= (const Term&, const Term&);
+    friend bool operator > (const Term&, const Term&);
+    friend bool operator >= (const Term&, const Term&);
+private:
+    // Data fields
+    int exponent, coefficient;
+};
+
+// Default Constructor
+inline Term::Term(int exp, int coeff) : exponent(exp), coefficient(coeff) {}
+
+// Less-Than Operator
+inline bool operator<(const Term& lhs, const Term& rhs) { return lhs.exponent < rhs.exponent; }
+
+// Less-Than or Equal operator
+inline bool operator<=(const Term& lhs, const Term& rhs) { return !(rhs < lhs); }
+
+// Greater-Than Operator
+inline bool operator>(const Term& lhs, const Term& rhs) { return lhs > rhs; }
+
+// Greater-Than or Equal Operator
+inline bool operator>=(const Term& lhs, const Term& rhs) { return !(lhs < rhs); }
+
 
 #endif
